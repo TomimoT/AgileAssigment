b4d1e9d1ccb9fa849b1199048922eac3f2f4e9a7
TomimoT <37384160+TomimoT@users.noreply.github.com>
2019-11-27 16:50:30 +0000

    Update HelloPython.py

--- HelloPython.py
+++ HelloPython.py
@@ -1,3 +1,13 @@
+import datetime
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+ 
   print("Hello World!")
+  now = datetime.datetime.now()
+  print("Current date and time is ")
+  print(now.strftime("%A, %d-%m-%Y : %H:%M"))
 
+  return 'hello, new concept'
